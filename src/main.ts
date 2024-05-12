d2640950850bd4ba18b3379ebe882f899aa711bf
aezazali1997 <aezazali.pro@gmail.com>
2024-05-12 14:27:40 +0500

    added cors and raw body for nest

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,{
+    cors:true,
+    rawBody:true
+  });
   await app.listen(process.env.PORT);
+  app.enableCors({
+    origin:'*',
+  })
 }
 bootstrap();
